4fa63c3d1832acec72253ef4c377e260a81778ba
Bis87 <pavel.rytvinsky@gmail.com>
2017-06-27 23:25:50 +0300

    static objects addition

--- landing/views.py
+++ landing/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from datetime import date
 # Create your views here.
+def landing(request):
+    name = 'Bis'
+    current_day = date.today()
+    return render(request, 'landing/landing.html', locals())
